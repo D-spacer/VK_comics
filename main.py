1328552a3798afd41939cbd40327f0e0a6503a18
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2023-02-13 03:57:52 +0300

    edited checking function

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from dotenv import load_dotenv
 
 
 def check_vk_response(response):
-    decoded_response = response.json()
-    if 'error' in decoded_response:
-        raise ConnectionError(f'Возникла ошибка {decoded_response["error"]["error_msg"]}')
+    if 'error' in response:
+        raise ConnectionError(f'Возникла ошибка {response["error"]["error_msg"]}')
 
 
 def get_random_comic_number():
@@ -38,8 +37,8 @@ def get_link_to_upload(group_id, access_token, api_version=5.131):
     }
     server_details_response = requests.get('http://api.vk.com/method/photos.getWallUploadServer',
                                            params=server_parameters)
-    check_vk_response(server_details_response)
     decoded_server_details_response = server_details_response.json()
+    check_vk_response(decoded_server_details_response)
     return decoded_server_details_response['response']['upload_url']
 
 
@@ -50,8 +49,8 @@ def upload_image(image_number_to_post, upload_link):
             'photo': file
         }
         image_upload_response = requests.post(url, files=files)
-    check_vk_response(image_upload_response)
     decoded_image_upload_response = image_upload_response.json()
+    check_vk_response(decoded_image_upload_response)
     return decoded_image_upload_response['server'], decoded_image_upload_response['photo'], \
            decoded_image_upload_response['hash']
 
@@ -69,8 +68,8 @@ def save_image(server, photo, hash_, group_id, access_token, api_version=5.131):
     image_save_response = requests.post(
         'http://api.vk.com/method/photos.saveWallPhoto',
         params=image_parameters)
-    check_vk_response(image_save_response)
     decoded_image_save_response = image_save_response.json()
+    check_vk_response(decoded_image_save_response)
     return decoded_image_save_response["response"][0]["owner_id"], decoded_image_save_response["response"][0]["id"]
 
 
@@ -84,8 +83,9 @@ def post_image(owner_id, id_, group_id, comment, access_token, api_version=5.131
                        f'{id_}',
         'v': api_version
     }
-    check_vk_response(requests.post('http://api.vk.com/method/wall.post',
-                  params=image_post_parameters))
+    image_post_resonse = requests.post('http://api.vk.com/method/wall.post',
+                  params=image_post_parameters)
+    check_vk_response(image_post_resonse.json())
 
 
 def main():
